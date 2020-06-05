b192d696f4b7b506da9d1814c733932e93ae2c27
Thomas Glenn <thomasgrantglenn@mac.com>
2020-06-04 23:55:24 -0600

    add 5 more js code scenarios like, triangle tracker, programming suggester, etc.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 
-// this is code to receive user info from select boxes:
+// 1. this is code to receive user info from select boxes:
 const beverage = $("#beverage").val();
 
-// this is code to receive user info from radio buttons:
+// 2. this is code to receive user info from radio buttons:
 const flavor = $("input:radio[name=flavor]:checked").val();
 
-// this is code to receive user info from a calendar:
+// 3. this is code to receive user info from a calendar:
 const dob = $("#born").val();
 
-// this is code to receive user info from a color wheel:
+// 4. this is code to receive user info from a color wheel:
 const favoriteColor = $("#color").val();
 
-// this is code receive and display user info from text fields:
+// 5. this is code receive and display user info from text fields:
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     const person1Input = $("input#person1").val();
@@ -34,7 +34,7 @@ $(document).ready(function() {
   });
 });
 
-// this is code from our dating game:
+// 6. this is code from our dating game:
 $(document).ready(function() {
   $("#profile").submit(function(event) {
     const gender = $("#gender").val();
@@ -62,3 +62,131 @@ $(document).ready(function() {
   });
 });
 
+// 7. this is logic for determining triangle types:
+$(document).ready(function() {
+  $("form#triangle").submit(function() {
+    const a = parseInt($("#sideA").val());
+    const b = parseInt($("#sideB").val());
+    const c = parseInt($("#sideC").val());
+    const aplusb = a + b;
+    const cplusa = a + c;
+    const cplusb = c + b;
+
+    if (aplusb >= c) {
+      if (a === b && c === b) {
+        alert("Equilateral");
+      } else if (a === b || c === b || a === c) {
+        alert("Isosceles");
+      } else {
+        alert("Scalene");
+      }
+    } else if (cplusa >= b) {
+      if (a === b && c === b) {
+        alert("Equilateral");
+    } else if (a === b || c === b || a === c) {
+      alert("Isosceles");
+    } else {
+      alert("Scalene");
+    }
+    } else if (cplusb >= a) {
+      if (a === b && c === b) {
+        alert("Equilateral");
+      } else if (a === b || c === b || a === c) {
+        alert("Isosceles");
+      } else {
+        alert("Scalene");
+      }
+    } else {
+      alert("not a triangle")
+    }
+  
+  });
+});
+
+// 8. this is code for addClass and removeClass - horoscope:
+$(document).ready(function() {
+  $("form#yourinfo").submit(function(event) {
+    const name = $("#inputName").val();
+    const band = $("#inputBand").val();
+    const horoscope = $("#inputHoroscope").val();
+    const food = $("#inputFood").val();
+
+    if (name === "") {
+      $("#yourNameDiv").addClass("has-error");
+      $("span#helpBlock1").text("Please enter your name");
+    }
+
+    if (band === "") {
+      $("#yourBandDiv").addClass("has-error");
+      $("span#helpBlock2").text("Please enter your favorite band");
+    }
+
+    if (horoscope === "1") {
+      $("#yourHoroscopeDiv").addClass("has-error");
+      $("span#helpBlock3").text("Please enter your sign");
+    }
+
+    if (food === "") {
+      $("#yourFoodDiv").addClass("has-error");
+      $("span#helpBlock4").text("Please enter your favorite food");
+    }
+
+    if (name !== "" && band !== "" && horoscope !== "1" && food !== "") {
+      $("form#yourInfo").toggle();
+      $("#everybodyLovesDevito").show();
+    }
+    event.preventDefault();
+  });
+});
+
+// 9. this is the programmingSuggester .js code:
+var add = function(age, sit, fail, life) {
+  return age + sit + fail + life;
+
+};
+
+$(document).ready(function() {
+$("form#profile").submit(function(event) {
+  event.preventDefault();
+
+  // Part A. This code will display the results of the quiz:
+
+  var age = parseInt($("input:radio[name=age]:checked").val());
+  var junk = parseInt($("input:radio[name=junk]:checked").val());
+  var sit = parseInt($("input:radio[name=sit]:checked").val());
+  var fail = parseInt($("input:radio[name=fail]:checked").val());
+  var life = parseInt($("input:radio[name=life]:checked").val());
+
+  var language = add(age, junk, sit, fail, life);
+ 
+ 
+  if (language >= 4) {
+    var result = ("C#");
+  } else if (language === 3) {
+    var result = ("JavaScript");
+  } else if (language === 2) {
+    var result = ("Python");
+  } else if (language <= 1) {
+    var result = ("Swift");
+  }
+  
+  $("#present").show();
+  $("#output2").text(result);
+
+  // Part B. This code will display the personal information of the user:
+
+  var name1 = $("input#firstName").val();
+  var name2 = $("input#lastName").val();
+  var address = $("input#address").val();
+  var phone = $("input#phone").val();
+ 
+  $(".firstName").text(name1);
+  $(".lastName").text(name2);
+  $(".address").text(address);
+  $(".phone").text(phone);
+
+  var info = text(name1, name2, address, phone);
+
+  $("#output1").text(info);
+});
+});
